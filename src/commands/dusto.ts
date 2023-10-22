9788ffa7df5537d43a4c5b7ad5411e5385c207db
dontdiecoding <spbuddave@gmail.com>
2023-10-22 12:15:47 -0500

    auto unban dusto

--- src/commands/dusto.ts
+++ src/commands/dusto.ts
@@ -6,5 +6,7 @@ export class Dusto {
   @Slash({ name: 'dusto', description: 'Bans the F-out of dusto'})
   async dusto(interaction: ChatInputCommandInteraction) {
     await interaction.guild?.members.ban('675792043926355980')
+    await interaction.reply('Dusto has been banned')
+    await interaction.guild?.members.unban('675792043926355980')
   }
 }
